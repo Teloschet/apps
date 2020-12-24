7b54fd963929f2b4a8d43f08f93cdbcc965268c4
Teloschet <frapgameroficial@hotmail.com>
2020-12-24 10:25:34 -0300

    APP LAYOUT FOR SHOP

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Christopher">
+
+    <title>Shop App</title>
+
+    <link rel="stylesheet" type="text/css" href="global.css">
+    </head>
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="imgBx">
+                <img src="running_shoes_PNG5816.png">
+            </div>
+            <div class="contentBx">
+                <h2>Nike Running</h2>
+                <div class="size">
+                    <h3>Size :</h3>
+                    <span>7</span>
+                    <span>6</span>
+                    <span>9</span>
+                    <span>10</span>
+                </div>
+                <div class="color">
+                    <h3>Color :</h3>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <a href="">Buy Now</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="card">
+            <div class="imgBx">
+                <img src="tenis-nike-bq3207-002-revolution-5-278b1b6b12429d8ce7a3ec6925c654c3.png">
+            </div>
+            <div class="contentBx">
+                <h2>Nike Revolution 5</h2>
+                <div class="size">
+                    <h3>Size :</h3>
+                    <span>7</span>
+                    <span>6</span>
+                    <span>9</span>
+                    <span>10</span>
+                </div>
+                <div class="color">
+                    <h3>Color :</h3>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <a href="">Buy Now</a>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="card">
+            <div class="imgBx">
+                <img src="tenis-nike-bq3204-001-revolution-5-695b06a860ec4767cd31ede44b4af154.png">
+            </div>
+            <div class="contentBx">
+                <h2>Nike Shoes</h2>
+                <div class="size">
+                    <h3>Size :</h3>
+                    <span>7</span>
+                    <span>6</span>
+                    <span>9</span>
+                    <span>10</span>
+                </div>
+                <div class="color">
+                    <h3>Color :</h3>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <a href="">Buy Now</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
